3614b5c29282a1a5d62b8358de3234ec081f6263
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2024-01-20 12:38:05 +0800

    add `pack up` button

---
+++ theme/src/js/toolsbar.js
@@ -0,0 +1,27 @@
+var clipboard = new ClipboardJS("#post .copy-button");
+clipboard.on("success" , function( event ) {
+    event.clearSelection();
+});
+clipboard.on( "error" , function( event ) {
+    console.info( event );
+});
+var divs = document.getElementsByClassName("highlight");
+var len = divs.length;
+for ( var i = 0 ; i < len ; i++ ){
+    var div = divs.item( i );
+    var target = "copy-" + i;
+    div.children.item( 0 ).id = target;
+    div.insertAdjacentHTML( "afterbegin" , `<div class=\"code-toolsbar\"><button class=\"copy-button\" type=\"button\" title=\"copy\" data-clipboard-target=\"#${target}\"><i class=\"fa-regular fa-clipboard\"></i></button><button class=\"packup-button\" type=\"button\" title=\"packup\"><i class=\"fas fa-angle-down\"></i></button></div>` );
+    var button = div.children.item( 0 ).children.item( 1 );
+    var icon = button.children.item( 0 );
+    var pre = div.children.item( 1 );
+    button.onclick = function(){
+        if ( pre.style.display === "none" ){
+            pre.style.display = "block";
+            icon.setAttribute( "class" , "fas fa-angle-down" );
+        }else{
+            pre.style.display = "none";
+            icon.setAttribute( "class" , "fas fa-angle-left" );
+        }
+    };
+}
